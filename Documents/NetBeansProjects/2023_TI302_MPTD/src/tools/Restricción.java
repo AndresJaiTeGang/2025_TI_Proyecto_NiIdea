31b8565a45c842fdb0d913690e798c13c9925dce
AndresJaiTeGang <andresm09872@gmail.com>
2023-09-22 10:48:40 -0600

    First commit

---
+++ Documents/NetBeansProjects/2023_TI302_MPTD/src/tools/Restricción.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+/**
+ *
+ * @author JaiTeGang
+ */
+public class Restricción {
+    
+    float a1;
+    float a2;
+    float b1;
+    int equ;
+    
+    
+    
+}
