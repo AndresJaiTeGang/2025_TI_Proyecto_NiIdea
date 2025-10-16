2d81ec3109a2d0a8a39189536bc425d28fa3ab3e
AndresJaiTeGang <andresm09872@gmail.com>
2025-10-16 14:36:36 -0600

    Modified atributes and extend the class from class Usuario

--- Documents/NetBeansProjects/NiIdea/src/main/java/domain/Alumno.java
+++ Documents/NetBeansProjects/NiIdea/src/main/java/domain/Alumno.java
@@ -1,68 +1,27 @@
 package domain;
 
 /**
- * Representa a un alumno dentro del proyecto NiIdea.
- * Contiene información básica como número de control y nombre.
+ * Representa a un alumno dentro del proyecto NiIdea. Contiene información
+ * básica como número de control y nombre.
  *
- * Proyecto: NiIdea
- * Materia: Taller de Investigación 1
- * Periodo: Agosto-Diciembre 2025
- * Docente: FJMP
- * Programador: Andres Mendoza
- * Fecha: 02/10/2025
+ * Proyecto: NiIdea Materia: Taller de Investigación 1 Periodo: Agosto-Diciembre
+ * 2025 Docente: FJMP Programador: Andres Mendoza Fecha: 02/10/2025
  *
  * @author Andres Mendoza
  * @version 1.0
  * @since 2025-10-02
  */
-public class Alumno {
-    private String NoControl;
-    private String Nombre;
+public class Alumno extends Usuario {
 
-    /**
-     * Establece el número de control del alumno.
-     *
-     * @param NoControl Número de control único del alumno.
-     */
-    public void setNoControl(String NoControl) {
-        this.NoControl = NoControl;
+    private String ndc;
+
+    public Alumno(String ndc, String nombre, String ndu, String clave) {
+        super(nombre, ndu, clave);
+        this.ndc = ndc;
     }
 
-    /**
-     * Establece el nombre del alumno.
-     *
-     * @param Nombre Nombre completo del alumno.
-     */
-    public void setNombre(String Nombre) {
-        this.Nombre = Nombre;
+    public String getNdc() {
+        return ndc;
     }
 
-    /**
-     * Obtiene el número de control del alumno.
-     *
-     * @return Número de control del alumno.
-     */
-    public String getNoControl() {
-        return NoControl;
-    }
-
-    /**
-     * Obtiene el nombre del alumno.
-     *
-     * @return Nombre del alumno.
-     */
-    public String getNombre() {
-        return Nombre;
-    }
-
-    /**
-     * Constructor que inicializa un objeto Alumno con su número de control y nombre.
-     *
-     * @param NoControl Número de control del alumno.
-     * @param Nombre Nombre del alumno.
-     */
-    public Alumno(String NoControl, String Nombre) {
-        this.NoControl = NoControl;
-        this.Nombre = Nombre;
-    }
 }
